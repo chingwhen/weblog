fda8884a8ad1394794e633f04da099652e6046a5
tanjianchuan <tanjianchuan@gmail.com>
2015-02-03 16:43:36 +0800

    在fork后，修改Global配置

--- blog/views.py
+++ blog/views.py
@@ -19,16 +19,13 @@ blog = Blueprint('blog', __name__,
 
 # Global
 USER = {
-    'name': u'马孔多',
-    'avatar': u'http://tp4.sinaimg.cn/1990786715/180/5642425207/1',
-    'avatar_large': u'http://whoop-upload.stor.sinaapp.com/1353599241_me.jpg',
-    'email': u'rongxiaosong@gmail.com',
-    'weibo': u'http://weibo.com/rongxs',
+      'name': u'chingwhen',
+    'avatar': u'https://avatars2.githubusercontent.com/u/2391463?v=3&s=460',
+    'avatar_large': u'https://avatars2.githubusercontent.com/u/2391463?v=3&s=460',
+    'email': u'tanjianchuan@gmail.com',
+    'weibo': u'http://weibo.com/chingwhen',
     'intro': u'''
-    马孔多，《百年孤独》的小镇，取这个名字并不代表我很文艺。正在读书的程序员，\
-    每天十几个小时坐在电脑前会压抑人的神经，所以酷爱户外运动，爬山最佳。此外，\
-    读书、电影也是必不可少。广招朋友，欢迎IT人士以及有各类爱好的朋友前来指教。\
-    Feel Free to Email Me: rongxiaosong@gmail.com
+    Feel Free to Email Me: tanjianchuan@gmail.com
     '''
 }
 #
